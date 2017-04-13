0f540d8f713e82fc4b81091142baf58eea9433a8
Kyle <Malithium@gmail.com>
2017-04-13 16:50:39 +0100

    added in TextBlob libraby to tag the markov keys and values

--- main.py
+++ main.py
@@ -1,66 +1,100 @@
 from random import randint
+from textblob import TextBlob
+import sys
+
+class MarkovKey:
+    def __init__(self, key):
+        self.key = key
+        self.tags = ""
+
+    def __str__(self):
+        return self.key
+
+    def __repr__(self):
+        return self.key
+
+    def __hash__(self):
+        return hash(self.key)
+
+    def __eq__(self, other):
+        return self.key == other.key
 
 class MarkovVal:
-	def __init__(self, word, weight):
-		self.word = word
-		self.weight = weight
-		
-	def setWeight(self, weight):
-		self.weight = weight
+    def __init__(self, word, weight):
+        self.word = word
+        self.tag = ""
+        self.weight = weight
+
+    def __str__(self):
+        return self.word
+
+    def __repr__(self):
+        return self.word
+        
+    def setWeight(self, weight):
+        self.weight = weight
 
 class Markov:
+    def __init__(self, corpus_path):
+        self.corpus = open(corpus_path, 'r')
+        self.corpusSize = self.getCorpusSize(corpus_path)
+        self.markov = self.generateMarkov()
 
-	def __init__(self, corpus_path):
-		self.corpus = open(corpus_path, 'r')
-		self.markov = self.generateMarkov()
-		
-	def generateMarkov(self):
-		tempStr = self.corpus.readlines()
-		for line in tempStr:
-			tempStrArr = line.split();
-			#generates a 2 key markov chain
-			for ind, word in enumerate(tempStrArr):
-				
-				#if the value does not exceed the array sixe
-				if ind + 2 < len(tempStrArr):
-					keyStr = tempStrArr[ind] + " " + tempStrArr[ind+1]
-					valueStr = MarkovVal(tempStrArr[ind + 2], 1)
-					
-					#if there is a variable called markov in the local scope
-					if 'markov' in locals():
-						#if the key already exists add it to value array
-						if keyStr in markov:
-							markov[keyStr].append(valueStr)
-						else:
-							markov[keyStr] = [valueStr]
-					else:
-						markov = {keyStr: [valueStr]}
-		return markov
-		
-	def createResponse(self, text):
-		sentence = ""
-		currKey = ""
-		inputs = text.split()
-		for i, w in enumerate(inputs):
-			currKey = inputs[i] + " " + inputs[i+1]
-			break;
+    def getCorpusSize(self, corpus_path):
+        with open(corpus_path) as f:
+            return sum(1 for _ in f)   
 
-		if currKey in self.markov:
-			sentence = currKey
-			while True:
-				values = self.markov[currKey]
-				val = values[randint(0,len(values)-1)]
-				newKey = currKey.split()[1] + " " + val.word
-				sentence += " " + val.word
-				currKey = newKey
-				if newKey not in self.markov:
-					break
-		print(sentence)
-		
-m = Markov('test.txt')
-m.createResponse('I am')
+    def generateMarkov(self):
+        sys.stdout.write("Step 1: Generating Markov Chain\n")
+        sys.stdout.flush()
+        tempStr = self.corpus.readlines()
+        i = 0;
+        for line in tempStr:
+            i+=1
+            sys.stdout.write("reading line " + str(i) + " of " + str(self.corpusSize) + "\r")
+            sys.stdout.flush()
+            blob = TextBlob(line)
+            for idx, val in enumerate(blob.tags):
+                sect = blob.tags[idx]
+                if idx + 2 < len(blob.tags):
+                    sect2 = blob.tags[idx+1]
+                    value = blob.tags[idx+2]
+                    keyStr = MarkovKey(sect[0] + " " + sect2[0])
+                    keyStr.tags = sect[0][0] + " " + sect2[0][0]
+                    valueStr = MarkovVal(value[0], 0.1)
+                    valueStr.tag = value[0][0]
+                    if 'markov' in locals():
+                        if keyStr in markov:
+                            markov[keyStr].append(valueStr)
+                        else:
+                            markov[keyStr] = [valueStr]
+                    else:
+                        markov = {keyStr: [valueStr]}
+        return markov
+'''
+            #generates a 2 key markov chain
+            for ind, word in enumerate(tempStrArr):
+                
+                #if the value does not exceed the array sixe
+                if ind + 2 < len(tempStrArr):
+                    keyStr = MarkovKey(tempStrArr[ind] + " " + tempStrArr[ind+1])
+                    valueStr = MarkovVal(tempStrArr[ind + 2], 1)
+                    
+                    #if there is a variable called markov in the local scope
+                    if 'markov' in locals():
+                        #if the key already exists add it to value array
+                        if keyStr in markov:
+                            markov[keyStr].append(valueStr)
+                        else:
+                            markov[keyStr] = [valueStr]
+                    else:
+                        markov = {keyStr: [valueStr]}
+        return markov'''
+
+        
+m = Markov('nietzsche.txt')
 print(m.markov)
-'''				
+'''                
 print(tempStr)
 print("-------------------------------------------------------------------------")
 print(markov)
